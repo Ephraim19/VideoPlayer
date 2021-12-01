c95760d3bb1ca56e1393f129f35cfeec81d44e20
Ephraim19 <machayoephraim@gmail.com>
2021-12-01 08:24:28 +0300

    initial project

---
+++ app/screens/video.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Video = (props) => {
+    return (
+        <View>
+            <Text>eph</Text>
+        </View>
+    )
+}
+
+export default Video
+
+
